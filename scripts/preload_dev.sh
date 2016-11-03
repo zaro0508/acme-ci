b8d032ab1d2ba9cdc43e39f89b90683821a3a2b8
Khai Do <zaro0508@gmail.com>
2016-11-03 16:55:02 -0700

    A demo ci project
    A demo ci project for scaling jenkins jobs talk.
    It uses vagrant to configure 2 VMs for the demo.

---
+++ scripts/preload_dev.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+#git clone https://github.com/vfarcic/books-ms.git
+
+set -e
+
+#cd books-ms
+#
+#docker build \
+#    -f Dockerfile.test \
+#    -t 10.100.198.200:5000/books-ms-tests \
+#    .
+#
+#docker push 10.100.198.200:5000/books-ms-tests
+#
+#docker-compose \
+#    -f docker-compose-dev.yml \
+#    run --rm tests
+#
+#docker build \
+#    -f Dockerfile \
+#    -t 10.100.198.200:5000/books-ms \
+#    .
+#
+#docker push 10.100.198.200:5000/books-ms
+#
+#docker pull mongo
+
+#docker pull jenkins
+
+#apt-get install -y openjdk-7-jdk
