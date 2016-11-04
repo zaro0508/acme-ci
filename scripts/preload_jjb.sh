960925947e2e2db32492a087cd6ebe572e9a1dad
Khai Do <zaro0508@gmail.com>
2016-11-04 15:36:32 -0700

    Revert "remove simple job"
    This reverts commit 482cd5ef76da09fd5d60cf1c423e9f17faa9d7d3.

--- scripts/preload_jjb.sh
+++ scripts/preload_jjb.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+sudo apt-get install -y git python-pip
+sudo pip install tox
 git clone https://git.openstack.org/openstack-infra/jenkins-job-builder /home/vagrant/jenkins-job-builder
 sudo chgrp -R vagrant /home/vagrant/jenkins-job-builder
 sudo chown -R vagrant /home/vagrant/jenkins-job-builder
